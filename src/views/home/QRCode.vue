d3ed8b3d5135cdd70eff455d0dca944c0457c8ee
wangyalijas <1114920411@qq.com>
2018-11-02 18:57:32 +0800

    问卷调查

--- src/views/home/QRCode.vue
+++ src/views/home/QRCode.vue
@@ -6,8 +6,7 @@
       width="20%">
       <img :src="qrCode" alt="二维码">
       <span slot="footer" class="dialog-footer">
-        <el-button @click="centerDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+        <el-button @click="visible = false">取 消</el-button>
       </span>
     </el-dialog>
   </div>
